b5810ec112c724e33d53abe343222e99362117fc
nannyy5333-beep <nannyy5333@gmail.com>
2025-10-16 17:07:29 +0500

    Delete postgres_db.py

--- postgres_db.py
+++
@@ -1,5 +0,0 @@
-# postgres_db.py (top-level)
-# Make legacy 'from postgres_db import ...' work from anywhere.
-from project.postgres_db import PostgresManager, get_db, execute, fetchone, fetchall
-
-__all__ = ["PostgresManager", "get_db", "execute", "fetchone", "fetchall"]
